a5357ab46882fe1f1ae66f30fbebc0251416961f
nicholas <nicholas_kerr1990@yahoo.com>
2023-06-20 01:25:54 +0100

    add styling to navbar component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,29 @@
+import { Link } from "react-router-dom";
 import "./NavBar.scss";
+import HomeIcon from "../../assets/capstone-icons/home-icon.svg";
+import TodoIcon from "../../assets/capstone-icons/todo-icon.svg";
+import ProfileIcon from "../../assets/capstone-icons/profile-icon.svg";
+import CreateIcon from "../../assets/capstone-icons/create-icon.svg";
+import StatsIcon from "../../assets/capstone-icons/stats-icon.svg";
 
 export default function NavBar () {
     return (
   <section className="navbar">
-      
+      <Link >
+        <img className="navbar__home" src={HomeIcon} alt="" />
+      </Link>
+      <Link >
+        <img className="navbar__todo" src={TodoIcon} alt="" />
+      </Link>
+      <Link >
+        <img className="navbar__create" src={CreateIcon} alt="" />
+      </Link>
+      <Link >
+        <img className="navbar__stats" src={StatsIcon} alt="" />
+      </Link>
+      <Link >
+        <img className="navbar__profile" src={ProfileIcon} alt="" />
+      </Link>
   </section>
     )
   };
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -7,7 +7,7 @@ import NavBar from "../../components/NavBar/NavBar";
 
 export default function LandingPage() {
   return (
-    <div>
+    <div className="landing-page">
       <Header />
       <DashBoard />
       <Todo />
